3612e95b0bf632bbafda2c3f00f88b155244fb21
bianduom <bianduom@umich.edu>
2019-10-20 11:55:59 -0400

    survey added

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -8,68 +8,117 @@
     </p>
     <div class="bv-example-row bv-example-row-flex-cols">
       <b-row class="my-4 justify-content-center">
-        <b-button v-b-modal.modal-center>Block #1</b-button>
+        <b-button :disabled="!this.b_show_1" v-b-modal.modal-center>Block #1</b-button>
       </b-row>
       <b-row class="my-4 justify-content-center">
-        <b-button v-b-modal.modal-center>Block #2</b-button>
+        <b-button :disabled="!this.b_show_2" v-b-modal.modal-center>Block #2</b-button>
       </b-row>
       <b-row class="my-4 justify-content-center">
-        <b-button v-b-modal.modal-center>Block #3</b-button>
+        <b-button :disabled="!this.b_show_3" v-b-modal.modal-center>Block #3</b-button>
+      </b-row>
+      <b-row class="my-4 justify-content-center">
+        <b-button v-b-modal.modal-prevent-closing>Experiment Survey</b-button>
+      </b-row>
+      <b-row class="my-4 justify-content-center">
+        Participant Name: {{this.form.name}}
       </b-row>
     </div>
 
-    <b-modal 
-      id="modal-center" 
-      size="xl"
-      centered title="Block #1"
-      v-model="show"
-      :hide-footer="true"
-      :header-bg-variant="headerBgVariant"
-      :header-text-variant="headerTextVariant"
-      :body-bg-variant="bodyBgVariant"
-      :body-text-variant="bodyTextVariant"
-      :footer-bg-variant="footerBgVariant"
-      :footer-text-variant="footerTextVariant"
-      :no-close-on-backdrop="true"
-      :no-close-on-esc="true"
-      :hide-header-close="true"
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Experiment Survey"
     >
-      <b-container class="bv-example-row">
-        <!-- <b-row>
-          <b-col  class="mx-2 box" :style="this.scenarioColor" ></b-col>
-          <b-col cols="6" class="bg-danger mx-2" style="height: 280px;" ></b-col>
-          <b-col cols="2" class="mx-2" style="height: 280px;">
-            <b-button v-on:click="nextUp" class="py-4 my-4" variant="outline-primary">Option #1</b-button>
-            <b-button v-on:click="nextUp" class="py-4 my-4" variant="outline-primary">Option #2</b-button>
-          </b-col>
-        </b-row> -->
-        <b-row class="justify-content-center align-items-center my-1">
-          <div style="position: absolute;">
-            <h1>1</h1>
-          </div>
-          <img :src="require('../retrollystimuli/Current Choice Square.png')" style="position: relative; width: 50%; height: 50%; top: 10px;"/>
-          <img :src="require('../retrollystimuli/Ex Avatar 001.png')" style="position: absolute; max-width:15%; max-height:15%; left: 30%; top: 25%;"/>
-          <img :src="require('../retrollystimuli/Arrows 1.png')" style="position: absolute;max-width:30%; max-height:30%; top: 18%;" />
-          <img :src="require('../retrollystimuli/You Full 1.png')" style="position: absolute;max-width:15%; max-height:15%; right: 30%; top: 25%;"/>
-        </b-row>
-        <b-row class="justify-content-center align-items-center mt-5 bt-5">
-          <img :src="require('../retrollystimuli/Current Choice Square.png')" style="position: relative; width: 50%; height: 50%; top: 10px;"/>
-          <img :src="require('../retrollystimuli/Ex Avatar 001.png')" style="position: absolute; max-width:15%; max-height:15%; left: 30%; bottom: 9%;"/>
-          <img :src="require('../retrollystimuli/Arrows 1.png')" style="position: absolute;max-width:30%; max-height:30%; bottom: 1%;" />
-          <img :src="require('../retrollystimuli/You Full 1.png')" style="position: absolute;max-width:15%; max-height:15%; right: 30%; bottom: 9%;"/>
-        </b-row>
-      </b-container>
+      <b-form @submit="onSubmit" @reset="onReset">
+
+      <b-form-group id="input-group-2" label="Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Date:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.date"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Gender:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.gender"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="RA Present:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.ra"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Age:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.age"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Number of Older Brother(s):" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.olderBro"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Number of Older Sister(s):" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.olderSis"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Number of Younger Brother(s):" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.youngerBro"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Number of Younger Sister(s):" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.youngerSis"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button class="mr-2" type="submit" variant="primary">Submit</b-button>
+      <b-button class="ml-2" type="reset" variant="danger">Reset</b-button>
+    </b-form>
     </b-modal>
+
+    <BlockOne @blockOneDone="blockOneFinished"/>
   </div>
 </template>
 
 <script>
-import gameCase from './gameCase.vue'
+import BlockOne from './blockOne.vue'
 
 export default {
   name: 'MainPage',
   components: {
-    gameCase,
+    BlockOne,
   },
   props: {
     msg: String
@@ -77,6 +126,9 @@ export default {
   data() {
     return {
       show: false,
+      b_show_1: true,
+      b_show_2: false,
+      b_show_3: false,
       variants: ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'],
       headerBgVariant: 'dark',
       headerTextVariant: 'light',
@@ -87,28 +139,58 @@ export default {
       colors:['red', 'yellow', 'brown', 'orange', 'black'],
       scenarioColor: `height: 280px; backgroundColor: red;`,
       index: 0,
-      configKonva: {
-        width: 200,
-        height: 200
-      },
-      configCircle: {
-        x: 100,
-        y: 100,
-        radius: 70,
-        fill: "red",
-        stroke: "black",
-        strokeWidth: 4
-      }
+      form: {
+          name: '',
+          date: '',
+          gender: '',
+          ra: '',
+          age: '',
+          olderBro: '',
+          olderSis: '',
+          youngerBro: '',
+          youngerSis: '',
+        },
+      formshow: true,
     }
   },
+  beforeMount() {
+  },
+  computed: {
+  },
   methods: {
-    nextUp () {
-      this.index += 1
-      if (this.index >= this.colors.length) {
-        this.index = 0
+    // form ---------------------------------------------------
+    onSubmit(evt) {
+      evt.preventDefault()
+      if(this.b_show_1 == false && this.b_show_2 == false && this.b_show_2 == false) {
+        this.b_show_1 = true
       }
-      this.scenarioColor = `height: 280px; backgroundColor: ${this.colors[this.index]};`
+      alert(JSON.stringify(this.form))
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.formshow = false
+      this.$nextTick(() => {
+        this.formshow = true
+      })
+    },
+    blockOneFinished(variable) {
+      this.b_show_1 = false
+      this.b_show_2 = true
+    },
+    blockTwoFinished(variable) {
+      this.b_show_2 = false
+      this.b_show_3 = true
+    },
+    blockThreeFinished(variable) {
+      this.b_show_3 = false
     }
+    
   }
 }
 </script>
---
+++ src/components/blockOne.vue
@@ -0,0 +1,388 @@
+<template>
+    <b-modal 
+      id="modal-center" 
+      size="xl"
+      centered title="Block #1"
+      v-model="show"
+      :hide-footer="true"
+      :header-bg-variant="headerBgVariant"
+      :header-text-variant="headerTextVariant"
+      :body-bg-variant="bodyBgVariant"
+      :body-text-variant="bodyTextVariant"
+      :footer-bg-variant="footerBgVariant"
+      :footer-text-variant="footerTextVariant"
+      :no-close-on-backdrop="true"
+      :no-close-on-esc="true"
+      :hide-header-close="true"
+    >
+      <b-container class="bv-example-row">
+        <b-row class="justify-content-center align-items-center my-1">
+          <b-progress style="width: 100%;" :value="this.current_progress" :max="this.max_avatar" show-progress animated></b-progress>
+        </b-row>
+        <b-row class="justify-content-center align-items-center my-1"
+        :style=this.prior_choice_style>
+          <div style="position: absolute; top: 19%; left: 49%;">
+            <h1 style="position: absolute;">
+              {{this.combinations[this.current_avatar].pr_p.p_first}}
+            </h1>
+          </div>
+          <div style="position: absolute;  top: 19%; right: 45%;">
+            <h1 style="position: absolute; color: #4B00FF;">
+              {{this.combinations[this.current_avatar].pr_p.a_first}}
+            </h1>
+          </div>
+          <div style="position: absolute; top: 39%; left: 49%;">
+            <h1 style="position: absolute;">
+              {{this.combinations[this.current_avatar].pr_p.p_second}}
+            </h1>
+          </div>
+          <div style="position: absolute; top: 39%; right: 45%;">
+            <h1 style="position: absolute; color: #4B00FF;">
+              {{this.combinations[this.current_avatar].pr_p.a_second}}
+            </h1>
+          </div>
+          <img :src="require('../retrollystimuli/Current Choice Square.png')" style="position: relative; width: 50%; height: 50%; top: 10px;"/>
+          <img @click="priorAvatar" :src="require(`../retrollystimuli/avatarsgalore/slice${this.avatar_list[this.current_avatar]}.png`)" style="position: absolute; max-width:15%; max-height:15%; left: 30%; top: 25%;"/>
+          <img :src="require(`../retrollystimuli/Arrows ${this.arrow_num}.png`)" style="position: absolute;max-width:30%; max-height:30%; top: 18%; left: 39%;" />
+          <img @click="helper" :src="require('../retrollystimuli/You Full 1.png')" style="position: absolute;max-width:15%; max-height:15%; right: 30%; top: 25%;"/>
+        </b-row>
+
+        <!-- Current Choice Block -->
+        <b-row class="justify-content-center align-items-center mt-5 bt-5" :style=this.current_choice_style>
+          <div style="position: absolute; top: 70%; left: 49%;">
+            <h1 style="position: absolute;">
+              {{this.combinations[this.current_avatar].a_p.p_first}}
+            </h1>
+          </div>
+          <div style="position: absolute;  top: 70%; right: 45%;">
+            <h1 style="position: absolute; color: #4B00FF;">
+              {{this.combinations[this.current_avatar].a_p.a_first}}
+            </h1>
+          </div>
+          <div style="position: absolute; top: 90%; left: 49%;">
+            <h1 style="position: absolute;">
+              {{this.combinations[this.current_avatar].a_p.p_second}}
+            </h1>
+          </div>
+          <div style="position: absolute; top: 90%; right: 45%;">
+            <h1 style="position: absolute; color: #4B00FF;">
+              {{this.combinations[this.current_avatar].a_p.a_second}}
+            </h1>
+          </div>
+          <img :src="require('../retrollystimuli/Prior Choice Square.png')" style="position: relative; width: 50%; height: 50%; top: 10px;"/>
+          <img @click="ChoiceOtherHelper" :src="require(`../retrollystimuli/avatarsgalore/slice${this.avatar_list[this.current_avatar]}.png`)" style="position: absolute; max-width:15%; max-height:15%; left: 30%; bottom: 9%;"/>
+          <img :src="require('../retrollystimuli/Arrows 1.png')" style="position: absolute;max-width:30%; max-height:30%; bottom: 1%; left: 39%;" />
+          <img @click="ChoiceSelfHelper" :src="require('../retrollystimuli/You Blank 1.png')" style="position: absolute;max-width:15%; max-height:15%; right: 30%; bottom: 9%;"/>
+          <div style="position: absolute; top: 80%; right: 35%;">
+            <h1 style="position: absolute; transform:translate(-33%,0px); color: #4B00FF;">
+              {{this.combinations[this.current_avatar].pl_p}}
+            </h1>
+          </div>
+        </b-row>
+      </b-container>
+    </b-modal>
+</template>
+
+<script>
+    export default {
+        name: 'BlockOne',
+        components: {
+
+        },
+        data() {
+            return {
+            show: false,
+            b_show_1: true,
+            variants: ['primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', 'dark'],
+            headerBgVariant: 'dark',
+            headerTextVariant: 'light',
+            bodyBgVariant: 'light',
+            bodyTextVariant: 'dark',
+            footerBgVariant: 'warning',
+            footerTextVariant: 'dark',
+            colors:['red', 'yellow', 'brown', 'orange', 'black'],
+            scenarioColor: `height: 280px; backgroundColor: red;`,
+            index: 0,
+            show_current: 0,
+            show_prior: 1,
+            arrow_num: '1',
+            avatar_num: '1',
+            avatar_list: this.shuffle(Array(1080).fill().map((x,i)=>i)),
+            current_avatar: 0,
+            current_progress: 0,
+            // How many games to run
+            max_avatar: 3,
+            avatar_choices: ["3", "2"],
+            player_payoff: ["3/2", "2", "5/2"],
+            avatar_payoff: [
+                {
+                p_first: "3",
+                a_first: "3",
+                p_second: "1",
+                a_second: "1",
+                },
+                {
+                p_first: "3",
+                a_first: "3",
+                p_second: "1",
+                a_second: "2",
+                },
+                ],
+            prior_payoff: [
+                // 3 3 1 1
+                {
+                p_first: "3",
+                a_first: "3",
+                p_second: "1",
+                a_second: "1",
+                },
+                // 3 3 1 2
+                {
+                p_first: "3",
+                a_first: "3",
+                p_second: "1",
+                a_second: "2",
+                },
+                // 3 3 2 1
+                {
+                p_first: "3",
+                a_first: "2",
+                p_second: "2",
+                a_second: "1",
+                },
+                // 3 3 2 2
+                {
+                p_first: "3",
+                a_first: "3",
+                p_second: "2",
+                a_second: "2",
+                },
+                // 1 3 3 1
+                {
+                p_first: "1",
+                a_first: "3",
+                p_second: "3",
+                a_second: "1",
+                },
+                // 1 3 3 2
+                {
+                p_first: "1",
+                a_first: "3",
+                p_second: "3",
+                a_second: "2",
+                },
+                // 2 3 3 1
+                {
+                p_first: "2",
+                a_first: "3",
+                p_second: "3",
+                a_second: "1",
+                },
+                // 2 3 3 2
+                {
+                p_first: "2",
+                a_first: "3",
+                p_second: "3",
+                a_second: "2",
+                },
+                // 8 
+                // 3 2 1 1
+                {
+                p_first: "3",
+                a_first: "2",
+                p_second: "1",
+                a_second: "1",
+                },
+                // 2 3 1 1
+                {
+                p_first: "2",
+                a_first: "3",
+                p_second: "1",
+                a_second: "1",
+                },
+                // 3 2 2 1
+                {
+                p_first: "3",
+                a_first: "2",
+                p_second: "2",
+                a_second: "1",
+                },
+                // 2 3 1 2
+                {
+                p_first: "2",
+                a_first: "3",
+                p_second: "1",
+                a_second: "2",
+                },
+                // 2 2 1 1
+                {
+                p_first: "2",
+                a_first: "2",
+                p_second: "1",
+                a_second: "1",
+                },
+                // 1 2 3 1
+                {
+                p_first: "1",
+                a_first: "2",
+                p_second: "3",
+                a_second: "1",
+                },
+                // 1 3 2 1
+                {
+                p_first: "1",
+                a_first: "3",
+                p_second: "2",
+                a_second: "1",
+                },
+                // 1 3 2 2
+                {
+                p_first: "1",
+                a_first: "3",
+                p_second: "2",
+                a_second: "2",
+                },
+                // 2 2 3 1
+                {
+                p_first: "2",
+                a_first: "2",
+                p_second: "3",
+                a_second: "1",
+                },
+                // 1 2 2 1
+                {
+                p_first: "1",
+                a_first: "2",
+                p_second: "2",
+                a_second: "1",
+                },
+                // 16
+                ],
+            combinations: [],
+            }
+        },
+        beforeMount() {
+            this.buildCombinations();
+        },
+        computed: {
+            prior_choice_style() {
+            return `opacity: ${this.show_prior}; transition: opacity 0.5s;`
+            },
+            current_choice_style() {
+            return `opacity: ${this.show_current}; transition: opacity 0.5s;`
+            },
+        },
+        methods: {
+            nextUp () {
+            this.index += 1
+            if (this.index >= this.colors.length) {
+                this.index = 0
+            }
+            this.scenarioColor = `height: 280px; backgroundColor: ${this.colors[this.index]};`
+            },
+            helper() {
+            alert('click the avatar!')
+            },
+            priorAvatar() {
+            this.show_current = 1;
+            this.arrow_num = this.combinations[this.current_avatar].a_c;
+            },
+            ChoiceOtherHelper() {
+            this.show_current = 0;
+            this.show_prior = 0;
+            let parent = this;
+            this.current_progress += 1;
+            setTimeout(function() { 
+                parent.current_avatar += 1;
+                if(parent.current_avatar == parent.max_avatar) {
+                parent.show = false;
+                alert('Block #1 finished')
+                parent.$emit('blockOneDone', true)
+                parent.b_show_1 = false;
+                }
+                parent.show_prior = 1; 
+                parent.arrow_num = '1';
+            }, 1000);
+            },
+            ChoiceSelfHelper() {
+            this.show_current = 0;
+            this.show_prior = 0;
+            let parent = this;
+            this.current_progress += 1;
+            console.log(this.combinations);
+            setTimeout(function() { 
+                parent.current_avatar += 1;
+                if(parent.current_avatar == parent.max_avatar) {
+                parent.show = false;
+                alert('Block #1 finished!');
+                parent.$emit('blockOneDone', true)
+                parent.b_show_1 = false;
+                }
+                parent.show_prior = 1; 
+                parent.arrow_num = '1';
+            }, 1000);
+            },
+            shuffle(array) {
+            var currentIndex = array.length, temporaryValue, randomIndex;
+
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+
+            return array;
+            },
+            buildCombinations() {
+            var i, k, j, o
+            for (i = 0; i < this.avatar_choices.length; i++) {
+                for (k = 0; k < this.player_payoff.length; k++) {
+                for (j = 0; j < this.avatar_payoff.length; j++) {
+                    for (o = 0; o < this.prior_payoff.length; o++) {
+                    var new_comb = {
+                        a_c: this.avatar_choices[i],
+                        pl_p: this.player_payoff[k],
+                        a_p: this.avatar_payoff[j],
+                        pr_p: this.prior_payoff[o],   
+                    }
+                    this.combinations.push(new_comb);
+                    }
+                }
+                }
+            }
+            this.combinations = this.shuffle(this.combinations)
+
+            },
+        }
+
+    }
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.b-col {
+  transition: background-color .3s;
+}
+</style>
