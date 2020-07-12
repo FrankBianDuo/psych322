f3e7facee69c10fe9ffb5d05eba37de341f8251f
bianduom <bianduom@umich.edu>
2020-07-12 19:39:34 -0400

    now the page challenges the paricipant to enter the CAPTCHA

--- src/components/tutorialPages/BotStopper.vue
+++ src/components/tutorialPages/BotStopper.vue
@@ -15,10 +15,21 @@
       :hide-header-close="true"
     >
       <b-container class="align-bottom" :style="this.windowsize"  >
-          <img :src="require('../../assets/Instructions/Bot_Stoppers/Bot Stopper 1.png')" style="width: 90%; height: auto; transform: translate(-50%, 0); margin-left: 50%;"/>
+          <img :src="require('../../assets/Instructions/Bot_Stoppers/Bot Stopper 1.png')" style="width: 70%; height: auto; transform: translate(-50%, 0); margin-left: 50%;"/>
+          <b-form>
+              <b-form-group id="input-group-1" label="What do you see?" label-for="input-1">
+                <b-form-input
+                id="input-1"
+                v-model="captcha"
+                required
+                placeholder="Enter the CAPTCHA here..."
+                ></b-form-input>
+            </b-form-group>
+          </b-form>
+          <!-- <b-button :v-if="!check_dev" variant="outline-primary" size="lg">Unlock</b-button> -->
       </b-container>
-        <b-button variant="outline-primary" size="lg">Back</b-button>
-        <b-button style="float: right;" @click="$bvModal.hide('modal-center-BotStopper')" variant="outline-primary" size="lg" v-b-modal.modal-center-Consent>Next</b-button>
+        <b-button :disabled="!check_robot" variant="outline-primary" size="lg">Back</b-button>
+        <b-button :disabled="!check_robot" style="float: right;" @click="$bvModal.hide('modal-center-BotStopper')" variant="outline-primary" size="lg" v-b-modal.modal-center-Consent>Next</b-button>
     </b-modal>
 </template>
 
@@ -31,9 +42,16 @@
         data() {
             return {
                 show: false,
+                captcha: "",
             }
         },
         computed: {
+            check_robot() {
+                return this.captcha == "73?q5&"
+            },
+            check_dev() {
+                return this.captcha == "m3M3"
+            }
         },
         methods: {
             
